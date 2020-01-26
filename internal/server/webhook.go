e2453fafba557e1e8c98f68f7d41ed5fd93a6e05
Chris Suszyński <ksuszyns@redhat.com>
2020-01-26 17:11:24 +0100

    Pretty print a webhook request

--- internal/server/webhook.go
+++ internal/server/webhook.go
@@ -5,15 +5,51 @@ import (
 	"github.com/wavesoftware/go-ensure"
 	"github.com/wavesoftware/go-kahle/internal/base"
 	"net/http"
+	"strings"
 )
 
 func (s *KahleServer) webhookHandler(writer http.ResponseWriter, req *http.Request) {
 	log := base.Logger()
-	log.Debugf("Received webhook: %v", req)
+	log.Debugf("Received request: %v", PrettyRequest{req: req})
 
-	writer.Header().Add("X-Server", fmt.Sprintf("Kahle/%s", base.Version))
-	writer.Header().Add("Content-Type", "application/json")
+	s.respondWithStatus(writer, "received")
+}
 
-	_, err := writer.Write([]byte("{\"status\": \"Received!\"}"))
+func (s *KahleServer) respondWithStatus(writer http.ResponseWriter, status string) {
+	headers := writer.Header()
+	headers.Add("X-Server", fmt.Sprintf("Kahle/%s", base.Version))
+	headers.Add("Content-Type", "application/json")
+
+	_, err := writer.Write([]byte(fmt.Sprintf("{\"status\": \"%s\"}", status)))
 	ensure.NoError(err)
+}
+
+// PrettyRequest can display your request in easy readable form
+type PrettyRequest struct {
+	req *http.Request
+}
+
+// String presents a request as a easy readable ascii representation
+func (p *PrettyRequest) String() string {
+	// Create return string
+	var request []string
+	// Add the request string
+	url := fmt.Sprintf("%v %v %v", p.req.Method, p.req.URL, p.req.Proto)
+	request = append(request, url) // Add the host
+	request = append(request, fmt.Sprintf("Host: %v", p.req.Host))
+	// Loop through headers
+	for name, headers := range p.req.Header {
+		name = strings.ToLower(name)
+		for _, h := range headers {
+			request = append(request, fmt.Sprintf("%v: %v", name, h))
+		}
+	}
+
+	// If this is a POST, add post data
+	if p.req.Method == "POST" {
+		ensure.NoError(p.req.ParseForm())
+		request = append(request, "\n")
+		request = append(request, p.req.Form.Encode())
+	} // Return the request as a string
+	return strings.Join(request, "\n")
 }
